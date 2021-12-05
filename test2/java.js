f8ea70ef206b473e4482d8bdaf7c5143a912a9d5
rovenkocorp <35662149+rovenkocorp@users.noreply.github.com>
2021-12-05 16:55:54 +0100

    Delete test2 directory

--- test2/java.js
+++
@@ -1,7 +0,0 @@
-import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
-
-var swiper = new Swiper('.swiper-container', {    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-                                             });
